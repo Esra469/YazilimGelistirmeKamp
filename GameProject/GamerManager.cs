48a1e0617f281661a8935dfe2105fe1493efc554
EsraÇimen <eesra8352@gmail.com>
2026-03-15 13:55:11 +0300

    Gamer project

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    //Hiçbir sınıf boş kalmamalı. Gamerservice somut kısmı 
+    internal class GamerManager : IGamerService
+    {
+        public void Add(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt oldu");
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt silindi");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Güncellendi");
+        }
+    }
+}
